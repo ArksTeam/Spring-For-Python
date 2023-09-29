62cad61256900d1292cc8060a48115ce59edbedc
Loong <admin@mc-m.net>
2023-09-29 21:06:12 +0800

    Update setup script.

--- setup.py
+++ setup.py
@@ -1,14 +1,21 @@
 import setuptools
 
+README = ''
+PACKAGE_LIST = []
+with open('README.md','r') as f:
+    README = f.read()
+with open('package_list.txt','r') as f:
+    PACKAGE_LIST = eval(f.read())
+
 setuptools.setup(
     name = "spring",
     version = "1.0",
     author = "ArksTeam | Jankie",
     author_email = "Jankie@edu.awaish.link",
     description = "",
-    long_description = "spring",
-    long_description_content_tpye = "spring",
-    packages = setuptools.find_packages(),
+    long_description = README,
+    zip_safe = False,
+    packages = PACKAGE_LIST,
     classifiers = [
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: Apache License",
