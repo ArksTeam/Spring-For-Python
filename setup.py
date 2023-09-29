64a939a6ceb9a50ca7dae96c7c1a7d3caf9f8c28
Loong <admin@mc-m.net>
2023-09-29 21:00:12 +0800

    Setup script.

---
+++ setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+    name = "spring",
+    version = "1.0",
+    author = "ArksTeam | Jankie",
+    author_email = "Jankie@edu.awaish.link",
+    description = "",
+    long_description = "spring",
+    long_description_content_tpye = "spring",
+    packages = setuptools.find_packages(),
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache License",
+        "Operating System :: OS Independent"
+    ]
+)
